dab3648513ba8c063428ca83ea86039f4c69d000
cross-ts <ts.kouhei@gmail.com>
2022-01-23 22:30:38 +0900

    cdk init

---
+++ cfn-macros/test/cfn-macros.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CfnMacros from '../lib/cfn-macros-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cfn-macros-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CfnMacros.CfnMacrosStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
