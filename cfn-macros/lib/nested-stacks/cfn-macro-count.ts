749beea550c6c01a8d4aa8323f9baa7b6956ea0b
cross-ts <ts.kouhei@gmail.com>
2022-01-26 00:36:56 +0900

    Constructを使用するように変更

--- cfn-macros/lib/nested-stacks/cfn-macro-count.ts
+++ cfn-macros/lib/nested-stacks/cfn-macro-count.ts
@@ -1,8 +1,14 @@
 import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CFnMacroConstruct } from '../constructs/cfn-macro-construct';
 
 export class CFnMacroCount extends NestedStack {
   constructor(scope: Construct, id: string, props?: NestedStackProps) {
     super(scope, id, props);
+
+    new CFnMacroConstruct(this, id, {
+      functionName: 'cfn-macro-count',
+      macroName: 'Count',
+    })
   }
 }
