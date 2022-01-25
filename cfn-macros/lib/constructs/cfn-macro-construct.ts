efaa3312240be18456e17786ecc2e43c2c497ed4
cross-ts <ts.kouhei@gmail.com>
2022-01-26 01:09:57 +0900

    IAM Roleを作るようにした

--- cfn-macros/lib/constructs/cfn-macro-construct.ts
+++ cfn-macros/lib/constructs/cfn-macro-construct.ts
@@ -2,35 +2,50 @@ import {
   aws_cloudformation as cloudformation,
   aws_lambda as lambda,
   aws_logs as logs,
+  aws_iam as iam,
   RemovalPolicy
 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export interface CFnMacroConstructProps {
-  functionName: string,
   macroName: string,
+  functionName: string,
+  functionRuntime?: lambda.Runtime,
+  functionHandler?: string,
 }
 
 export class CFnMacroConstruct extends Construct {
+
+  readonly lambdaRole: iam.Role;
+  readonly lambdaFunction: lambda.Function;
+
   constructor(scope: Construct, id: string, props: CFnMacroConstructProps) {
     super(scope, id);
 
-    const fn = new lambda.Function(scope, props.functionName, {
+    this.lambdaRole = new iam.Role(scope, `${props.functionName}-Role`, {
+      roleName: props.functionName,
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    })
+
+    this.lambdaFunction = new lambda.Function(scope, props.functionName, {
       functionName: props.functionName,
       code: lambda.Code.fromAsset(`lambda/${props.functionName}/src`),
-      runtime: lambda.Runtime.PYTHON_3_9,
-      handler: 'index.handler',
+      runtime: props.functionRuntime ?? lambda.Runtime.PYTHON_3_9,
+      handler: props.functionHandler ?? 'index.handler',
+      role: this.lambdaRole,
     });
 
-    new logs.LogGroup(scope, `${props.functionName}-LogGroup`, {
-      logGroupName: `/aws/lambda/${fn.functionName}`,
+    const logGroup = new logs.LogGroup(scope, `${props.functionName}-LogGroup`, {
+      logGroupName: `/aws/lambda/${props.functionName}`,
       retention: logs.RetentionDays.ONE_DAY,
       removalPolicy: RemovalPolicy.DESTROY,
     })
+    logGroup.grantWrite(this.lambdaFunction)
 
     new cloudformation.CfnMacro(scope, props.macroName, {
-      functionName: fn.functionArn,
+      functionName: this.lambdaFunction.functionArn,
       name: props.macroName,
+      logGroupName: logGroup.logGroupName,
     })
   }
 }
