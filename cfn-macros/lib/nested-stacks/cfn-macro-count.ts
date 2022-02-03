dbe6534f3c3f3cd0f3d8d66ae544157e2de5e993
cross-ts <ts.kouhei@gmail.com>
2022-02-03 23:08:24 +0900

    Add CFnMacroProps

--- cfn-macros/lib/nested-stacks/cfn-macro-count.ts
+++ cfn-macros/lib/nested-stacks/cfn-macro-count.ts
@@ -2,13 +2,16 @@ import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CFnMacroConstruct } from '../constructs/cfn-macro-construct';
 
+export interface CFnMacroCountProps extends NestedStackProps {
+}
+
 export class CFnMacroCount extends NestedStack {
-  constructor(scope: Construct, id: string, props?: NestedStackProps) {
+  constructor(scope: Construct, id: string, props?: CFnMacroCountProps) {
     super(scope, id, props);
 
     new CFnMacroConstruct(this, id, {
-      functionName: 'cfn-macro-count',
       macroName: 'Count',
+      functionName: 'cfn-macro-count',
     })
   }
 }
